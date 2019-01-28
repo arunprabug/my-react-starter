f95c5e62a63fb288cdc4adf128da432fddb6ca27
Arunprabu Ganesan <Arunprabu_Ganesan@ad.infosys.com>
2019-01-28 20:02:22 +0000

    changes to demo branching

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Home
           </a>
         </header>
       </div>
