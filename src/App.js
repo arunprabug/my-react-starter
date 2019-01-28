fc52617b62f097a72f7e5689ed33e015aecc136d
Arunprabu Ganesan <Arunprabu_Ganesan@ad.infosys.com>
2019-01-28 19:55:06 +0000

    first commit for react starter project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 class App extends Component {
   render() {
@@ -8,9 +8,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
